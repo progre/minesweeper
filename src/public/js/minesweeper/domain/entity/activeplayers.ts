965397af6377aaf03ce974d262b42a55d9d2cebb
progre <djyayutto@gmail.com>
2013-10-15 07:45:18 +0900

    bugfix

--- src/public/js/minesweeper/domain/entity/activeplayers.ts
+++ src/public/js/minesweeper/domain/entity/activeplayers.ts
@@ -6,18 +6,27 @@ import cifs = require('./../../../minesweeper-common/domain/entity/interfaces');
 export = ActivePlayers;
 class ActivePlayers extends ee2.EventEmitter2 {
     private centralPlayer: number;
-    private items: cifs.IHash<cifs.IPlayer> = {};
+    private items: cifs.IHash<cifs.IPlayer> = null;
+    private emitter: ee2.EventEmitter2;
 
     get(id: number): cifs.IPlayer {
         return this.items[id];
     }
 
     setEmitter(emitter: ee2.EventEmitter2) {
-        emitter.on('moved', (obj: iv.IMoveDTO) => {
+        this.emitter = emitter;
+    }
+
+    setPlayers(items: cifs.IHash<cifs.IPlayer>) {
+        this.items = items;
+        Object.keys(items).forEach(id => {
+            super.emit('player_added', { id: id, player: items[id] });
+        });
+        this.emitter.on('moved', (obj: iv.IMoveDTO) => {
             this.items[obj.id].coord = cdxo.toCoord(obj.coord);
             super.emit('player_moved', { id: obj.id, coord: this.items[obj.id].coord });
         });
-        emitter.on('player_activated', (obj: { id: number; player: iv.IPlayerDTO }) => {
+        this.emitter.on('player_activated', (obj: { id: number; player: iv.IPlayerDTO }) => {
             var player = cdxo.toPlayer(obj.player);
             this.items[obj.id] = player;
             super.emit('player_added', { id: obj.id, player: player });
@@ -25,17 +34,10 @@ class ActivePlayers extends ee2.EventEmitter2 {
                 super.emit('central_player_selected', obj.id);
             }
         });
-        emitter.on('player_deactivated', (obj: { id: number; player: iv.IPlayerDTO }) => {
+        this.emitter.on('player_deactivated', (obj: { id: number; player: iv.IPlayerDTO }) => {
             var player = this.items[obj.id];
             delete this.items[obj.id];
             super.emit('player_removed', obj.id);
-        });
-    }
-
-    setPlayers(items: cifs.IHash<cifs.IPlayer>) {
-        this.items = items;
-        Object.keys(items).forEach(id => {
-            super.emit('player_added', { id: id, player: items[id] });
         });
     }
 
